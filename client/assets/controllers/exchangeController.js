2f1fee69bc528881e6c308679c2a62d1c07b08cf
antcho92 <antcho92@gmail.com>
2016-11-22 16:53:10 -0800

    fixed join exchange form

--- client/assets/controllers/exchangeController.js
+++ client/assets/controllers/exchangeController.js
@@ -1,12 +1,12 @@
 app.controller('exchangeController', ['$scope', 'exchangeFactory', '$routeParams', function($scope, eF, $routeParams) {
   var self = this;
   this.customQuestions = [
-    {questionId: 1}
+    {id: 1}
   ];
   // dynamically add settings input in form
   this.addQuestion = function() {
     var newQuestionNum = this.customQuestions.length + 1;
-    this.customQuestions.push({'questionId': newQuestionNum})
+    this.customQuestions.push({'id': newQuestionNum})
   }
   this.create = function() {
     this.newExchange.customQuestions = this.customQuestions;
